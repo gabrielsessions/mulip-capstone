577b02342bc21f4d4967d8cfb3344971b0b5a9ac
Gabriel Sessions <gsess7732@gmail.com>
2024-11-15 03:24:42 -0500

    Cookie Clicker Game Done

--- flask-server/app.py
+++ flask-server/app.py
@@ -1,11 +1,14 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
 from flask_cors import CORS
+import os
 
 app = Flask(__name__, template_folder='templates', static_folder='static')
 socketio = SocketIO(app, cors_allowed_origins="*", supports_credentials=True)
 
 CORS(app, supports_credentials=True)
+
+cookieClickerScore = 0
 
 @app.route('/')
 def index():
@@ -13,8 +16,16 @@ def index():
 
 @socketio.on('message')
 def handle_message(msg):
-    print('Received message: ' + msg)
-    emit('response', 'Server received: ' + msg, broadcast=True)
+    global cookieClickerScore
+    print('Received cookie click!', cookieClickerScore)
+    cookieClickerScore += 1
+    emit('response', str(cookieClickerScore), broadcast=True)
 
+@socketio.on('connect')
+def send_global_score():
+    print("Connection!")
+    emit('response', cookieClickerScore)
+
+port_number = os.environ["PORT"] if "PORT" in os.environ else 8080
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    socketio.run(app, port=port_number)
