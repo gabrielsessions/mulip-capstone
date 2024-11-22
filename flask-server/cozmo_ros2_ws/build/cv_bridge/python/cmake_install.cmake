23d0717cedc718246d5d6476b3661024a5013041
Marvin <marvin@mulip.cs.tufts.edu>
2024-11-21 23:09:04 -0500

    robot moves

---
+++ flask-server/cozmo_ros2_ws/build/cv_bridge/python/cmake_install.cmake
@@ -0,0 +1,68 @@
+# Install script for directory: /home/marvin/cozmo_ros2_ws/src/vision_opencv/cv_bridge/python
+
+# Set the install prefix
+if(NOT DEFINED CMAKE_INSTALL_PREFIX)
+  set(CMAKE_INSTALL_PREFIX "/home/marvin/cozmo_ros2_ws/install/cv_bridge")
+endif()
+string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
+
+# Set the install configuration name.
+if(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
+  if(BUILD_TYPE)
+    string(REGEX REPLACE "^[^A-Za-z0-9_]+" ""
+           CMAKE_INSTALL_CONFIG_NAME "${BUILD_TYPE}")
+  else()
+    set(CMAKE_INSTALL_CONFIG_NAME "")
+  endif()
+  message(STATUS "Install configuration: \"${CMAKE_INSTALL_CONFIG_NAME}\"")
+endif()
+
+# Set the component getting installed.
+if(NOT CMAKE_INSTALL_COMPONENT)
+  if(COMPONENT)
+    message(STATUS "Install component: \"${COMPONENT}\"")
+    set(CMAKE_INSTALL_COMPONENT "${COMPONENT}")
+  else()
+    set(CMAKE_INSTALL_COMPONENT)
+  endif()
+endif()
+
+# Install shared libraries without execute permission?
+if(NOT DEFINED CMAKE_INSTALL_SO_NO_EXE)
+  set(CMAKE_INSTALL_SO_NO_EXE "1")
+endif()
+
+# Is this installation the result of a crosscompile?
+if(NOT DEFINED CMAKE_CROSSCOMPILING)
+  set(CMAKE_CROSSCOMPILING "FALSE")
+endif()
+
+# Set default install directory permissions.
+if(NOT DEFINED CMAKE_OBJDUMP)
+  set(CMAKE_OBJDUMP "/usr/bin/objdump")
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/cv_bridge/environment" TYPE FILE FILES "/home/marvin/cozmo_ros2_ws/build/cv_bridge/python/ament_cmake_environment_hooks/pythonpath.sh")
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/cv_bridge/environment" TYPE FILE FILES "/home/marvin/cozmo_ros2_ws/build/cv_bridge/python/ament_cmake_environment_hooks/pythonpath.dsv")
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib/python3.10/site-packages/cv_bridge-2.1.3-py3.10.egg-info" TYPE DIRECTORY FILES "/home/marvin/cozmo_ros2_ws/build/cv_bridge/python/ament_cmake_python/cv_bridge/cv_bridge.egg-info/")
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib/python3.10/site-packages/cv_bridge" TYPE DIRECTORY FILES "/home/marvin/cozmo_ros2_ws/src/vision_opencv/cv_bridge/python/cv_bridge/" REGEX "/[^/]*\\.pyc$" EXCLUDE REGEX "/\\_\\_pycache\\_\\_$" EXCLUDE)
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+  execute_process(
+        COMMAND
+        "/usr/bin/python3.10" "-m" "compileall"
+        "/home/marvin/cozmo_ros2_ws/install/cv_bridge/lib/python3.10/site-packages/cv_bridge"
+      )
+endif()
+
---
+++ flask-server/cozmo_ros2_ws/build/opencv_tests/colcon_command_prefix_setup_py.sh
@@ -0,0 +1,2 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/marvin/cozmo_ros2_ws/install/cv_bridge/share/cv_bridge/package.sh"
