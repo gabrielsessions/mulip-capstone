382e422b2e58f9988131dd18f1ecf4e6f0059385
Gabriel Sessions <gsess7732@gmail.com>
2025-04-14 17:32:33 -0400

    Websocket connection

--- user-frontend/index.js
+++ user-frontend/index.js
@@ -1,3 +1,14 @@
+const socket = io('https://cozmokart.site', {
+  transports: ['websocket'],
+});
+
+function sendAction(msg) {
+  console.log(msg);
+  console.log(socket);
+  socket.send(msg);
+}
+
+
 const joystick = document.getElementById('joystick');
 const container = document.getElementById('joystick-container');
 const output = document.getElementById('output');
@@ -28,6 +39,7 @@ function scheduleJoystickLog(x, y) {
 
     // Only log if changed from last logged value
     if (cleanX !== lastLoggedX || cleanY !== lastLoggedY) {
+      sendAction(`X: ${cleanX}, Y: ${cleanY}`)
       console.log(`Joystick HOLD -> X: ${cleanX}, Y: ${cleanY}`);
       lastLoggedX = cleanX;
       lastLoggedY = cleanY;
@@ -72,6 +84,7 @@ function resetJoystick() {
   line.setAttribute('y2', centerY);
 
   // Immediately log reset to center
+  sendAction(`X: 0.0, Y: 0.0`);
   console.log(`Joystick RESET -> X: 0.0, Y: 0.0`);
   lastLoggedX = 0;
   lastLoggedY = 0;
@@ -92,3 +105,5 @@ joystick.addEventListener('pointerup', (e) => {
   dragging = false;
   resetJoystick();
 });
+
+
