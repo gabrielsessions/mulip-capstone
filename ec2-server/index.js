f8d88e5a49c67e115af3889cf3daf3195536b907
Ubuntu <ubuntu@ip-172-31-91-89.ec2.internal>
2025-04-15 19:57:56 +0000

    Added real collision detection

--- ec2-server/index.js
+++ ec2-server/index.js
@@ -101,7 +101,7 @@ io.on("connection", (socket) => {
       setTimeout(() => {
         blockMsgs = false;
       }, 500);
-      io.emit("X: 0.0, Y: 0.0")
+      io.emit("message", "X:0, Y:0")
       console.log("STOP -- COLLISION")
     }
     else if (data && !blockMsgs) {
